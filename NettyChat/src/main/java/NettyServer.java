5ada3b4b95b5b2e9cb3eda7949a5e7ed20949193
yuxuejian <csu_yuxuejian@163.com>
2017-11-18 16:50:57 +0800

    引入jar

--- NettyChat/src/main/java/NettyServer.java
+++ NettyChat/src/main/java/NettyServer.java
@@ -1,5 +1,10 @@
+import io.netty.bootstrap.ServerBootstrap;
+
 
 public class NettyServer {
+	
+	public static void start(){
+	}
 
 	public static void main(String[] args) {
 		System.out.println("netty chat");
